7129cef5166a9312497d5f015044532b7c524820
Mark Miranda <notmarkmiranda@gmail.com>
2016-05-01 08:09:33 -0600

    rewrite routes.rb for verb paths & urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,38 @@ Rails.application.routes.draw do
 
   root to: "pages#splash"
 
-  resources :items, only: [:index, :show]
-  resources :users, only: [:new, :create]
-  resources :cart_items, only: [:create, :destroy, :update, :edit]
-  resources :orders, only: [:index, :create]
+  # resources :items, only: [:index, :show]
+  get '/items', to: 'items#index'
+  get '/items/:id', to: 'items#show', as: 'item'
 
-  namespace "admin" do
-    resources :items
-    resources :users, only: [:show]
-  end
+  # resources :users, only: [:new, :create]
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create', as: 'users'
+
+  # resources :cart_items, only: [:edit]
+  post '/cart_items', to: 'cart_items#create'
+  delete '/cart_items/:id', to: 'cart_items#destroy'
+  patch '/cart_items/:id', to: 'cart_items#update', as: 'cart_item'
+  get '/cart_items/:id', to: 'cart_items#edit', as: 'edit_cart_item'
+
+  # resources :orders, only: [:index, :create]
+  get '/orders', to: 'orders#index', as: 'orders'
+  post '/orders/', to: 'orders#create'
+
+  # namespace "admin" do
+  #   resources :items
+  #   resources :users, only: [:show]
+  # end
+  get '/admin/items', to: 'admin/items#index', as: 'admin_items'
+  post '/admin/items', to: 'admin/items#create'
+  get '/admin/items/new', to: 'admin/items#new', as: 'new_admin_item'
+  get '/admin/items/:id/edit', to: 'admin/items#edit', as: 'edit_admin_item'
+  get '/admin/items/:id', to: 'admin/items#show', as: 'admin_item'
+  patch '/admin/items/:id', to: 'admin/items#update'
+  delete '/admin/items/:id', to: 'admin/items#destroy'
+  get '/admin/users/:id', to: 'admin/users#show', as: 'admin_user'
+
+
 
   get "/order", to: "orders#show"
   get "/dashboard", to: "users#show"
